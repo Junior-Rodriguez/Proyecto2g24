406c2e04a047579a3756f8c009a825cc2ed88921
Junior Rodriguez <rodrijunior1518@gmail.com>
2022-09-14 17:16:26 -0500

    primer commit del proyecto

---
+++ src/main/java/com/ciclo3/proyecto2g24/Proyecto2g24.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.ciclo3.proyecto2g24;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Proyecto2g24 {
+
+    public static void main(String[] args) {
+        System.out.println("Logica proyecto");
+    }
+}
